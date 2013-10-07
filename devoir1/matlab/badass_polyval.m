8afb1e97d3bf05c64bf48f36e04924c57366ffc4
Benoît Legat <benoit.legat@gmail.com>
2013-10-07 10:10:12 +0200

    Do Q2.3 and fix Q3

--- devoir1/matlab/badass_polyval.m
+++
@@ -1,8 +0,0 @@
-% evalue le polynome qui a comme coefficients coef au point x
-% coef(1) * x^3 + coef(2) * x^2 + coef(1) * x
-function p = badass_polyval (coefs, x)
-p = 0;
-for coef = coefs
-    p = p * x + coef;
-end
-end
---
+++ devoir1/matlab/polyval_recurrence.m
@@ -0,0 +1,20 @@
+function p = polyval_recurrence (alpha, beta, x)
+% POLYVAL_RECURRENCE Evalue tous les polynomes p_k en un point x en O(n)
+%
+%  [p] = polyval_recurrence(alpha, beta, x) renvoie p respectant la recurrence
+%         p_k(x) = (x - alpha_k) * p_{k-1}(x) - beta_k^2 * p_{k-2}(x)
+%  ou alpha = [alpha_1 alpha_2 ... alpha_n], beta = [beta_2 beta_3 ... beta_n]
+%  et p = [p_0 p_1 p_2 ... p_n].
+%  On a donc alpha_k = p(k), beta_k = beta(k-1) et p_k = p(k+1).
+
+n = length(alpha);
+p = zeros(1, n);
+
+p(1) = 1;
+p(2)= x - alpha(1);
+
+for k = 2:n
+  p(k+1)= (x - alpha(k)) * p(k) - (beta(k-1))^2 * p(k-1);
+end
+
+end
