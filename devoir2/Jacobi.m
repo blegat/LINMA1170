36efbf0f829403239f74de16595862bd4c90dc10
Benoît Legat <benoit.legat@gmail.com>
2013-11-01 15:51:30 +0100

    Fix indenting

--- devoir2/Jacobi.m
+++ devoir2/Jacobi.m
@@ -1,6 +1,4 @@
 function [X, i] = Jacobi(A, b, eps)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
 
 D = diag(diag(A));
 L = tril(A) - D;
@@ -12,11 +10,10 @@ n=2000;
 x1 = zeros(length(A), 1);
 x2 = zeros(length(A), 1);
 while i<n && err>eps
-x2 = D\(-(L+U)*x1 + b);
-err = norm(x2-x1);
-x1=x2;
-i=i+1;
+    x2 = D\(-(L+U)*x1 + b);
+    err = norm(x2-x1);
+    x1=x2;
+    i=i+1;
 end
 X=x2;
 end
-
