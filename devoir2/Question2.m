741b5983bc43b671cb7a39bc69013c24670ecacb
Benoît Legat <benoit.legat@gmail.com>
2013-11-02 16:57:33 +0100

    Refactor q3 with q2

--- devoir2/Question2.m
+++ devoir2/Question2.m
@@ -1,15 +1,15 @@
-function [i1, i2] = Question2(L, a, iterator)
+function [i1, i2] = Question2(Img, a, iterator)
 %   L = image received
 %   a, k = coef T ( a/k sould be between 0 and 255)
 %
 
 k=abs(1+2*a);
 
-[M N] = size(L);
+[M N] = size(Img);
 TL = get_T(a,k,M);
 TR = get_T(a,k,N);
 
-[i1, i2, X] = doubleSolver(L, TL, eye(M), TR, eye(N), iterator, 'Q2', a, k);
+[i1, i2, X] = doubleSolver(Img, TL, eye(M), 0, TR, eye(N), 0, iterator, 'Q2', a, k);
 
 %NORM2 = sqrt(sum(sum((X-L).^2)))
 
@@ -18,8 +18,8 @@ emax=0;
 deltaMax=0; deltaMax2=0;
 for i=1:M
     for j=1:N
-        if norm(X(i,j)-L(i,j))>emax
-            emax=norm(X(i,j)-L(i,j));
+        if norm(X(i,j)-Img(i,j))>emax
+            emax=norm(X(i,j)-Img(i,j));
             deltaMax=Delta(i,j);
         end
         if norm(Delta(i,j))>deltaMax2
--- devoir2/doubleSolver.m
+++ devoir2/doubleSolver.m
@@ -1,41 +1,35 @@
-function [i1, i2, X] = doubleSolver (L, A1, B1, A2, B2, iterator, dir, a, k)
+function [i1, i2, X] = doubleSolver (Img, A1, B1, C1, A2, B2, C2, iterator, dir, a, k)
 % Solve
-%   A1 Y  = B1 (L + Delta)   (1)
-%   A2 X' = B2 Y'            (2)
+%   A1 Y  = B1 (Img + Delta) + C1 * mean(Img + Delta)   (1)
+%   A2 X' = B2 Y'            + C2 * mean(Img + Delta)   (2)
 % where Delta is a normal distribution of mean 0 and variance 1.
 % It uses Jacobi if iterator == 'Jacobi' or else it uses Gauss-Seidel
 % It stores the images at 'dir/..._a.png'.
 % i1 is the number of iterations used to solve (1) and i2 to solve (2)
 
-save_image(L, sprintf('%s/original_%d', dir, a*100));
+save_image(Img, sprintf('%s/original_%d', dir, a*100));
 
-[M N] = size(L);
-Delta = randn(M,N); % Ok mean = 0 variance = 1
-TL = get_T(a,k,M);
-TR = get_T(a,k,N);
-A = TL*L*TR; % initial blurred image
+[M N] = size(Img);
 
-save_image(A, sprintf('Q2/blurred_%d',a*100));
+TL = get_T(a, k, M);
+TR = get_T(a, k, N);
+IB = TL * Img * TR; % initial blurred image
+save_image(IB, sprintf('Q2/blurred_%d', a*100));
 
-Ad = (A+Delta); % blurred image with perturbation
+Delta = randn(M, N); % Ok mean = 0 variance = 1
+IBN = (IB + Delta); % blurred image with perturbation
+save_image(IBN, sprintf('Q2/noise_%d', a*100));
 
-save_image(Ad, sprintf('Q2/noise_%d',a*100));
-
-Y=zeros(M,N);
-X=zeros(N,M);
-
-%tic()
+meanIBN = mean(mean(IBN));
 
 if strcmp(iterator,'Jacobi')
-    [Y i1] = Jacobi(A1, B1 * Ad);
-    [X i2] = Jacobi(A2, B2 * Y');
+    [Y i1] = Jacobi(A1, B1 * IBN + C1 * meanIBN);
+    [X i2] = Jacobi(A2, B2 * Y' + C2 * meanIBN);
 else
-    [Y i1] = GaussSeidel(A1, B1 * Ad);
-    [X i2] = GaussSeidel(A2, B2 * Y');
+    [Y i1] = GaussSeidel(A1, B1 * IBN + C1 * meanIBN);
+    [X i2] = GaussSeidel(A2, B2 * Y' + C2 * meanIBN);
 end
 
-%toc()
-
 X=X';
-save_image(X, sprintf('Q2/unblurred_%d',a*100));
+save_image(X, sprintf('Q2/unblurred_%d', a*100));
 end
