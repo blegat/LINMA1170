e6bc12ef09234c9581621fd116f77a67709d32fb
Benoît Legat <benoit.legat@gmail.com>
2013-11-03 16:49:27 +0100

    Improve q3

--- devoir2/q2.m
+++ devoir2/q2.m
@@ -2,9 +2,9 @@ clear all;
 load devoir2_image;
 for a = [0.2 0.4 0.45 0.5]
     fprintf('%.2f', a);
-    [i1 i2] = Question2(I, a, 'Jacobi');
+    [i1 i2] = Question2(I, a, 'Jacobi', 1);
     fprintf(' & %d & %d', i1, i2);
-    [i1 i2] = Question2(I, a, 'Gauss-Seidel');
+    [i1 i2] = Question2(I, a, 'Gauss-Seidel', 1);
     fprintf(' & %d & %d', i1, i2);
     fprintf('\\\\\n');
 end
--- devoir2/q3r.m
+++ devoir2/q3r.m
@@ -1,5 +1,22 @@
 clear all;
 load devoir2_image;
-for a = 0.2:0.2:0.4
-    find_r(I, a, 'Jacobi');
+Img = I;
+r = linspace(0,0.6,12);
+fail_e = 1600;
+a = [0.1 0.2 0.25 0.3 0.4 0.45]';
+e = zeros(numel(a),numel(r));
+col = ['b' 'r' 'm' 'c' 'y' 'g'];
+for ia = 1:numel(a)
+    e = r;
+    for i = 1:numel(r)
+        [i1 i2 X] = Question3(Img, a(ia), r(i), 'Jacobi', 0);
+        if i1 == 500 || i2 == 500
+            e(ia,i) = fail_e;
+        else
+            e(ia,i) = norm(X - Img, 'fro');
+        end
+    end
+    plot(r, e(ia,:), col(ia)); hold on;
 end
+legend('a=0.1', 'a=0.2', 'a=0.25', 'a=0.3', 'a=0.4', 'a=0.5');
+saveas(gcf, 'Q3/ar', 'png');
