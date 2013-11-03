8f33d91c04c413837750f51cf3acc5f6228342a7
Benoît Legat <benoit.legat@gmail.com>
2013-11-03 22:06:07 +0100

    Improve q3

--- devoir2/q3.m
+++ devoir2/q3.m
@@ -1,10 +1,10 @@
 clear all;
 load devoir2_image;
-for a = [0.2]
+for a = [0.2 0.4 0.45 0.5]
     fprintf('%.2f', a);
-    [i1 i2] = Question3(I, a, 1, 'Jacobi');
+    [i1 i2] = Question3(I, a, 0.6, 'Jacobi', 2);
     fprintf(' & %d & %d', i1, i2);
-    [i1 i2] = Question3(I, a, 1, 'Gauss-Seidel');
+    [i1 i2] = Question3(I, a, 0.6, 'Gauss-Seidel', 2);
     fprintf(' & %d & %d', i1, i2);
     fprintf('\\\\\n');
 end
