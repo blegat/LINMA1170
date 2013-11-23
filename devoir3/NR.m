c9a9e568558c498b2619291f252ef2820cbc9a15
ark2812 <arnaud.cerckel@gmail.com>
2013-11-23 13:31:19 +0100

    Avant d'aller manger

--- devoir3/NR.m
+++ devoir3/NR.m
@@ -1,12 +1,12 @@
 function [ y] = NR(y, w, W, A)
 %NR Newton-Raphson implementation for non symetric matrix
-%   
+%
 
-G = W'*A*(w+W*y)-w'*A*(w+W*y)*y;
 
 for i = 1:500
-JG = W'*A*W-w'*A*w-2*w'*A*W*y; % function G's jacobian 
-y = -(JG\G)+y;
+    G = W'*A*(w+W*y)-w'*A*(w+W*y)*y;
+    JG = W'*A*W-w'*A*w-2*w'*A*W*y; % function G's jacobian
+    y = -(JG\G)+y;
 end
 
 end
