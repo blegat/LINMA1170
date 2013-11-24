afc4b1c23a18d86b1271b874a719ec878f7b01e2
ark2812 <arnaud.cerckel@gmail.com>
2013-11-25 00:56:11 +0100

    Rayleigh sym changée

--- devoir3/ConvDominanteS.m
+++ devoir3/ConvDominanteS.m
@@ -1,19 +1,22 @@
     %%
 
 A=[2 4 6;4 2 1; 6 1 2];
+n = 15;
+x = zeros(n);
 x(1:3) = [1/sqrt(3) 1/sqrt(3) 1/sqrt(3)]';
-x(4:6) = [1/sqrt(3) -1/sqrt(3) 1/sqrt(3)]';
-x(7:9) = [1/sqrt(3) -1/sqrt(3) -1/sqrt(3)]';
-muVect = [-10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9 10 ];
-muFinal = zeros(length(x),length(muVect));
+x(4:6) = [0.7071  0  0.7071]';
+x(7:9) = [1/sqrt(3) -1/sqrt(3) 1/sqrt(3)]';
+x(10:12) =[1 0 0]';
+x(13:15) = [1/sqrt(3) -1/sqrt(3) -1/sqrt(3)]';
+%muVect = [-10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9 10 ];
+rhoFinal = zeros(n/3);
 
-for j=0:3:6
-    for i=1:length(muVect)
-        
-        muFinal(j+1:j+3,i) = RQIS(A,x(j+1:j+3)',muVect(i));
-        
+for j=0:3:n
+         
+    if(mod(j,3) == 0)
+        rhoFinal((j/3)+1) = RQIS(A,x(j+1:j+3)');
     end
 end
 
-surf(muVect,[1, 1, 1, 2, 2, 2, 3, 3, 3],muFinal);
+plot([1, 2, 3, 4, 5],rhoFinal);
 
